1a636cb244cf32cbda187a8f2ffd401cb3b6bb9d
Brandon Gutierrez <bran09123@gmail.com>
2024-01-25 22:25:39 -0600

    added domain_objective key

--- text/books/comptia_security_practice_tests/json/oi.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import json
-with open("./data.json", "r+") as json_file:
-    questions = json.load(json_file)
-    domain_objectives = questions.keys()
-    for domain_objective in questions:
-        for question in questions[domain_objective]:
-            question["question_id"] = str(uuid.uuid4())
-    json.dump(questions, json_file)
